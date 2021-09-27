9700c8acd4b22ef2a8169d31249d1089aa9f33e5
Jmatop <91370388+Jmatop@users.noreply.github.com>
2021-09-27 11:59:37 +0200

    Update script2.bash

--- Calipso/script2.bash
+++ Calipso/script2.bash
@@ -1,8 +1,6 @@
 #!/bin/bash
-apt install tcpdump
-apt install timelimit
-apt install apache2
-bash pruebaleds1.sh && bash pruebaleds1.sh && bash pruebaleds1.sh
+
+# bash pruebaleds1.sh && bash pruebaleds1.sh && bash pruebaleds1.sh
 
 sudo rm /var/www/html/collectors/salida*.data
 
@@ -12,8 +10,8 @@ clear
 echo -e "\e[95mEscaneando Red.Por favor espere mientras trabajamos...\e[0m"
 sudo touch ataque.txt
 
-timelimit -t7 tcpdump  > escaneo.txt && bash lectoricmp.bash && bash lectorudp.bash && bash lectortcp.bash &&  bash lectorhttp.bash && bash lectorARP.bash &&
-bash analizador.bash |cut -c 1-10 
+timelimit -t7 tcpdump  > escaneo.txt && bash lectoricmp.bash && bash lectorudp.bash && bash lectortcp.bash &&  bash lectorhttp.bash && bash lectorARP.bash 
+# bash analizador.bash |cut -c 1-10 
 cat escaneo.txt | wc -l > /var/www/html/collectors/salidaPaquetes.data
 cat escaneo.txt | grep -c ICMP > /var/www/html/collectors/salidaICMP.data
 cat escaneo.txt | grep -c DHCP > /var/www/html/collectors/salidaDHCP.data
@@ -24,7 +22,7 @@ cat escaneo.txt | grep -c ARP  > /var/www/html/collectors/salidaARP.data
 
 
 
-bash analizador.bash
+# bash analizador.bash
 sleep 4s
 echo -e "\e[96mReiniciando.\e[0m"
 sleep 0.4
