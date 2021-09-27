77c7a761e5be12471f85ab932096835802e25b68
Jmatop <91370388+Jmatop@users.noreply.github.com>
2021-09-27 11:41:33 +0200

    Add files via upload

---
+++ Calipso/install.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get -y install python && sudo apt-get -y install apache2 && sudo apt-get -y install timelimit && sudo apt-get -y install tcpdump && sudo apt-get -y install net-tools && cd .. && sudo cp -r html /var/www/
