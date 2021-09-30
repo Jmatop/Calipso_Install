cb1cf0b57b05b27e344f8f04939db59dfe94e31c
Jmatop <91370388+Jmatop@users.noreply.github.com>
2021-09-30 19:24:14 +0200

    Update install.bash

--- Calipso/install.bash
+++ Calipso/install.bash
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sudo apt-get -y install python && sudo apt-get -y install apache2 && sudo apt-get -y install timelimit && sudo apt-get -y install tcpdump && sudo apt-get -y install net-tools 
+sudo apt-get -y install python && sudo apt-get -y install apache2 && sudo apt-get -y install timelimit && sudo apt-get -y install tcpdump && sudo apt-get -y install net-tools && sudo apache2ctl configtest && sudo apache2ctl configtest
+ && sudo systemctl restart apache2 && sudo ufw app list && sudo ufw app info "Apache Full"
+
+
+
+ 
