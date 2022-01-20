160a75211e52b9e62f701f4206a42d84d28233f1
Jmatop <91370388+Jmatop@users.noreply.github.com>
2022-01-20 16:12:11 +0000

    Update install.bash

--- Calipso/install.bash
+++ Calipso/install.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt-get -y install python && apt -y install php && sudo apt-get -y install apache2 && sudo apt-get -y install timelimit && sudo apt-get -y install tcpdump && sudo apt-get -y install net-tools && sudo apache2ctl configtest && sudo apache2ctl configtest && sudo systemctl restart apache2 && sudo ufw app list && sudo ufw app info "Apache Full"
+sudo apt-get -y install python && apt -y install php && sudo apt-get -y install apache2 && sudo apt-get -y install timelimit && sudo apt-get -y install tcpdump && sudo apt-get -y install net-tools && sudo apache2ctl configtest && sudo apache2ctl configtest && sudo systemctl restart apache2 && sudo ufw app list && touch calipso && echo "#!/bin/bash" > calipso && echo "xterm -e 'cd /home/calipso/Calipso/ && bash script2.bash' &" > calipso && sudo chmod +x calipso && sudo mv calipso /usr/bin && sudo ufw app info "Apache Full"
