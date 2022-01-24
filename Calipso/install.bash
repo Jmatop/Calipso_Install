8aaca17d505cbcd7dd9ea4a44e8cdf7d67f1eec2
Jmatop <91370388+Jmatop@users.noreply.github.com>
2022-01-24 14:30:33 +0000

    Update install.bash

--- Calipso/install.bash
+++ Calipso/install.bash
@@ -12,6 +12,7 @@ sudo systemctl restart apache2
 sudo ufw app list
 touch calipso 
 echo "#!/bin/bash" > calipso 
+echo "xdg-open http://localhost" > calipso
 echo "xterm -e 'cd $HOME/Calipso/Scripts/ && bash script2.bash' &" > calipso 
 sudo chmod +x calipso
 sudo mv calipso /usr/bin
